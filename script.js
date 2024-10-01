0278d0d07fbf1f74a5180186c4176b0a5b23ecbf
Zoz171 <zizo15.m@gmail.com>
2024-10-01 18:11:01 +0300

    Added Calculator Logic (Too lazy to fix 0's overflows)

---
+++ script.js
@@ -0,0 +1,60 @@
+let expression = "";
+let oper = "";
+let result = 0;
+let buttons = document.querySelectorAll(".btn");
+let screen = document.querySelector("#screen");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        oper = button.textContent;
+        // console.log(oper);
+        executeButton();
+    })
+})
+
+function clear(){
+    screen.textContent = 0;
+    expression = "";
+}
+
+function print(e){
+    if(e.length > 17) {
+        e = e.slice(e.length - 17);
+        console.log("overflow"); 
+    }
+    screen.textContent = e;
+}
+
+function executeButton(){
+    if(oper == "AC") clear();
+    else if(oper == "=") operate();
+    else {
+        if(oper == "Ans") expression += result;
+        else expression += oper;
+        print(expression);
+    }
+}
+
+function operate(){
+    try{
+        result = eval(expression);
+    }
+    catch(e){
+        if(e instanceof SyntaxError){
+            print("Syntax Error");
+            return;
+        }
+    }
+    finally{
+        if(result == Infinity)
+            print("Math Error");
+        else {
+            result = result.toFixed(2);
+            print(result);
+            expression = result;
+        }
+    }
+}
+
+
+// print("ZZZZ");
